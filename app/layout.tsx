f464c131bc49d0a4ddff9c957f28e155b40939f7
Fossen <simen.fossen@capgemini.com>
2026-02-20 12:43:36 +0100

    add workflows and write on tfstate article

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,7 +3,7 @@ import Navbar from "../components/Navbar";
 import Footer from "../components/Footer";
 
 export const metadata = {
-  title: "My Blog",
+  title: "Fozz",
 };
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
