4d8195ea3103ce3a9395a673b50e574e5648efac
Simen Fossen <simen.fosen@lyse.net>
2026-02-23 20:43:20 +0100

    change export to out for nextjs static website support

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,13 @@
 import createMDX from '@next/mdx'
+import type { NextConfig } from 'next'
 
-/** @type {import('next').NextConfig} */
 const nextConfig = {
   pageExtensions: ['js', 'jsx', 'ts', 'tsx', 'md', 'mdx'],
-}
+  output: 'export',
+  images: {
+    unoptimized: true,
+  },
+} satisfies NextConfig
 
 export default createMDX({
   extension: /\.(md|mdx)$/,
