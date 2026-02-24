496a48c94ec542032bb122a74d79ca9f7a6d6e89
Simen Fossen <simen.fosen@lyse.net>
2026-02-24 11:17:56 +0100

    add tf destroy and add to article

--- .github/Terraform/locals.tf
+++ .github/Terraform/locals.tf
@@ -1,16 +1,13 @@
 locals {
-  # Make sure the name is lower case
-  env = lower(var.env)
-
   # Your base project name
   base = "fozz"
 
   # Resource Group Name (may contain dashes)
-  rg_name = "${local.base}-RG-${local.env}"
+  rg_name = "${local.base}-RG"
 
     # Resource Group Name (may contain dashes)
-  strg_name = "${local.base}strg${local.env}"
+  strg_name = "${local.base}strg"
 
-  frontdoor_name = "${local.base}-frontdoor-${local.env}"
+  frontdoor_name = "${local.base}-frontdoor"
 
 }
